5ee25ff1410f1b12b7b1928cd6965e75885fd195
JeongJunSoo94 <Jssoft94@gmail.com>
2023-07-20 15:43:38 +0900

    Initial commit

---
+++ Source/ProjectJG/ProjectJG.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectJG.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectJG, "ProjectJG" );
---
+++ Source/ProjectJG/ProjectJGGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectJGGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTJG_API AProjectJGGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
